0a8fbfce881d41aa35f68af42dc613d56b644571
Kristina047 <ch.drobot@gmail.com>
2024-12-18 18:43:35 +0300

    дз объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Author pushkin = new Author("Александр", " Пушкин");
+        Author turgenev = new Author("Иван", " Тургенев");
+
 
         Book alex = new Book("Сказка о царе Салтане", pushkin, 1831);
+        alex.setYear(1832);
+
         System.out.println(alex.title);
         System.out.println(alex.author);
-        System.out.println(alex.year + " год");
-    }
+        System.out.println(alex.getYear() + " год");
+        System.out.println();
 
-    public static class Book {
-        private String title;
-        private Author author;
-        private int year;
-
-        public Book(String title, Author author, int year) {
-            this.title = title;
-            this.author = author;
-            this.year = year;
-        }
-
-        public String getName() {
-            return this.title;
-        }
-
-        public Author getAge() {
-            return this.author;
-        }
-
-        public void setAge(int year) {
-            this.year = year;
-        }
+        Book tur = new Book("Записки охотника", turgenev, 1852);
+        System.out.println(tur.title);
+        System.out.println(tur.author);
+        System.out.println(tur.year + " год");
     }
 
     public static class Author {
@@ -40,6 +26,7 @@ public class Main {
         public String toString() {
             return firstname + lastname;
         }
+
         public String getName() {
             return this.firstname;
         }
@@ -51,11 +38,42 @@ public class Main {
         public Author(String firstname, String lastname) {
             this.firstname = firstname;
             this.lastname = lastname;
-
-
         }
     }
+
+        public static class Book {
+            private String title;
+            private Author author;
+            private int year;
+
+
+            public Book(String title, Author author, int year) {
+                this.title = title;
+                this.author = author;
+                this.year = year;
+            }
+
+            public String getTitle() {
+                return this.title;
+            }
+
+            public Author getAuthor() {
+                return this.author;
+            }
+
+            public int getYear() {
+                return this.year;
+            }
+
+            public int setYear(int year) {
+                this.year = year;
+                return this.year;
+            }
+        }
+
 }
+
+
 
 
 
